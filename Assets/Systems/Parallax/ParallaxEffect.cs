dfcfa63d94c5cfbe2b66b2f6c10fddd0fd25c7c6
oskuro <oskurot@gmail.com>
2025-02-19 10:04:57 +0100

    added some vbackgrounds sprites. removed some old placeholder sprites

--- Assets/Systems/Parallax/ParallaxEffect.cs
+++ Assets/Systems/Parallax/ParallaxEffect.cs
@@ -2,7 +2,7 @@ using System;
 using UnityEngine;
 
 namespace se.nondescriptgames.utility
-{ 
+{
 
     [SelectionBase]
     public class ParallaxEffect : MonoBehaviour
@@ -10,7 +10,7 @@ namespace se.nondescriptgames.utility
 
         [Header("Near")]
         [SerializeField] Transform layerNear;
-        [SerializeField] [Range(0f,1f)] float parallaxNearHorizontal = 0.7f;
+        [SerializeField][Range(0f, 1f)] float parallaxNearHorizontal = 0.7f;
         Vector3 startPosNear = Vector3.zero;
         [Header("Mid")]
         [SerializeField] Transform layerMid;
@@ -22,6 +22,10 @@ namespace se.nondescriptgames.utility
         Vector3 startPosFar = Vector3.zero;
 
         Camera cam;
+
+        [SerializeField] bool horizontalParallax = true;
+        [SerializeField] bool verticalParallax = true;
+
         void Start()
         {
             if(layerNear == null || layerMid == null || layerFar == null) 
@@ -38,20 +42,39 @@ namespace se.nondescriptgames.utility
 
         void Update()
         {
-            UpdateHorizontalPosition(layerNear, parallaxNearHorizontal, startPosNear);
-            UpdateHorizontalPosition(layerMid, parallaxMidHorizontal, startPosMid);
-            UpdateHorizontalPosition(layerFar, parallaxFarHorizontal, startPosFar);
+            if (horizontalParallax)
+            {
+                UpdateHorizontalPosition(layerNear, parallaxNearHorizontal, startPosNear);
+                UpdateHorizontalPosition(layerMid, parallaxMidHorizontal, startPosMid);
+                UpdateHorizontalPosition(layerFar, parallaxFarHorizontal, startPosFar);
+            }
+
+            if (verticalParallax)
+            {
+                UpdateVerticalPosition(layerNear, parallaxNearHorizontal, startPosNear);
+                UpdateVerticalPosition(layerMid, parallaxMidHorizontal, startPosMid);
+                UpdateVerticalPosition(layerFar, parallaxFarHorizontal, startPosFar);
+            }
         }
 
         private void UpdateHorizontalPosition(Transform layer, float factor, Vector3 startPos)
         {
-            Vector3 Position = cam.transform.position;
-            float Temp = Position.x * (1 - factor);
-            float Distance = Position.x * factor;
+            Vector3 position = cam.transform.position;
+            float distance = position.x * factor;
 
-            Vector3 NewPosition = new Vector3(startPos.x + Distance, layer.position.y, layer.position.z);
+            Vector3 newPosition = new Vector3(startPos.x + distance, layer.position.y, layer.position.z);
 
-            layer.position = NewPosition;
+            layer.position = newPosition;
+        }
+
+        private void UpdateVerticalPosition(Transform layer, float factor, Vector3 startPos)
+        {
+            Vector3 position = cam.transform.position;
+            float distance = position.y * factor;
+
+            Vector3 newPosition = new Vector3(layer.position.x, startPos.y - distance, layer.position.z);
+
+            layer.position = newPosition;
         }
     }
 }
