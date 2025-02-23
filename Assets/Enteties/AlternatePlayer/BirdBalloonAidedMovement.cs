185981718c4848f169be44d283099a5a28677caa
oskuro <oskurot@gmail.com>
2025-02-23 11:15:55 +0100

    fixed alternate movement so it feels more smooth
    set up animations for alternate movement

--- Assets/Enteties/AlternatePlayer/BirdBalloonAidedMovement.cs
+++ Assets/Enteties/AlternatePlayer/BirdBalloonAidedMovement.cs
@@ -12,6 +12,19 @@ public class BirdBalloonAidedMovement : MonoBehaviour
     public Animator PlayerAnimator { get; private set; }
     private SpriteRenderer _spriteRenderer;
     InputAction _moveAction;
+    public bool IsGrounded { get; private set; }
+    float _rayDistance = 1f;
+    [SerializeField] Vector2 _groundCheckSize;
+    [SerializeField] LayerMask _groundLayer;
+    [SerializeField] LayerMask _balloonLayer;
+
+
+    // Animator hashes to improve speed 
+    int _flapHash;
+    int _jumpingHash;
+    int _groundedHash;
+
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -21,6 +34,19 @@ public class BirdBalloonAidedMovement : MonoBehaviour
         PlayerAnimator = GetComponentInChildren<Animator>();
 
         _spriteRenderer = GetComponentInChildren<SpriteRenderer>();
+
+        // calculate player height
+        BoxCollider2D collider = GetComponent<BoxCollider2D>();
+        if (collider != null)
+        {
+            var playerHeight = collider.bounds.max.y;
+            var offset = collider.offset.y;
+            _rayDistance = playerHeight / 2f - offset;
+        }
+
+        _flapHash = Animator.StringToHash("Flap");
+        _jumpingHash = Animator.StringToHash("Jumping");
+        _groundedHash = Animator.StringToHash("IsGrounded");
     }
 
     // Update is called once per frame
@@ -36,16 +62,30 @@ public class BirdBalloonAidedMovement : MonoBehaviour
         {
             _spriteRenderer.flipX = true;
         }
+
+        CheckGrounded();
+        SetAnimations();
+    }
+
+    private void SetAnimations()
+    {
+
+        
     }
 
     void FixedUpdate()
     {
         float move = _moveAction.ReadValue<float>();
-        if(move == 0)
-            return;
-        
-        if(_previousFlyTime + _flyInterval < Time.time)
+        if(move == 0) 
         {
+            PlayerAnimator.SetBool(_jumpingHash, !IsGrounded);
+            return;
+        }
+        
+        bool flying = _previousFlyTime + _flyInterval < Time.time;
+        if(flying)
+        {
+            PlayerAnimator.SetTrigger(_flapHash);            
             Fly(move);
             _previousFlyTime = Time.time;
         }
@@ -54,12 +94,30 @@ public class BirdBalloonAidedMovement : MonoBehaviour
 
     private void Fly(float move)
     {
-        
         Vector2 moveMagnitude = new Vector2(move * horizontalSpeed, verticalSpeed);
         Rb2D.AddForce(moveMagnitude, ForceMode2D.Impulse);
 
-        //Rb2D.linearVelocity = Vector2.ClampMagnitude(Rb2D.linearVelocity, 10f);
         Rb2D.linearVelocityX = Mathf.Clamp(Rb2D.linearVelocity.x, -horizontalSpeed, horizontalSpeed);
         Rb2D.linearVelocityY = Mathf.Clamp(Rb2D.linearVelocity.y, -verticalSpeed, verticalSpeed);
     }
+
+    private void CheckGrounded()
+    {
+        RaycastHit2D hit = Physics2D.BoxCast(transform.position, _groundCheckSize, 0f, Vector2.down, _rayDistance, _groundLayer);
+        IsGrounded = hit.collider != null;
+        PlayerAnimator.SetBool(_groundedHash, IsGrounded);
+
+        hit = Physics2D.BoxCast(transform.position, _groundCheckSize, 0f, Vector2.down, _rayDistance, _balloonLayer);
+        
+        if (hit.collider == null){return;}
+        
+        Debug.Log("Balloon hit");
+        
+        if(!hit.collider.gameObject.TryGetComponent<Health>(out Health health)) { return; }
+        
+        Debug.Log("Balloon hit with health");
+        health.TakeDamage(10);
+
+    }
+
 }
