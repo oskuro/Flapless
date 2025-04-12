b5d00909e46cbaab539e5be7b562ae7fb82c5077
oskuro <oskurot@gmail.com>
2025-04-12 15:19:20 +0200

    resizing chunks

--- Assets/Systems/ChunkManager/ChunkManager.cs
+++ Assets/Systems/ChunkManager/ChunkManager.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
 
@@ -5,27 +6,34 @@ public class ChunkManager : MonoBehaviour
 {
     [SerializeField] GameObject _starterChunk;
     [SerializeField] GameObject[] _levelChunks;
+    List<GameObject> chunks = new List<GameObject>();
 
     float _chunkYSize = 32f;
     Vector2 _currentChunkPos = Vector2.zero;
 
+    Camera mainCam;
 
     void Start()
     {
+        mainCam = Camera.main;
+
         SpawnChunk(_starterChunk);
         SpawnChunk(_levelChunks[Random.Range(0, _levelChunks.Length)]);
-        
-
     }
 
     void SpawnChunk(GameObject prefab) 
     {
-        Instantiate(prefab, _currentChunkPos, Quaternion.identity);
+        var chunk = Instantiate(prefab, _currentChunkPos, Quaternion.identity, transform);
+        chunks.Add(chunk);
         _currentChunkPos.y += _chunkYSize;
     }
 
     void Update()
     {
-        
+        if(mainCam.transform.position.y > _currentChunkPos.y - _chunkYSize)
+        {
+            Debug.Log($"Current Chunk Pos: {_currentChunkPos.y}");
+            SpawnChunk(_levelChunks[Random.Range(0, _levelChunks.Length)]);
+        }
     }
 }
