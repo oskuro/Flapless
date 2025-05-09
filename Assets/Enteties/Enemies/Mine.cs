1ac3da5c906bca839ba12d4900f1a867eb01dc95
Lukas <lukas.isaksson@gmail.com>
2025-05-09 15:47:21 +0200

    mine is now functional

--- Assets/Enteties/Enemies/Mine.cs
+++ Assets/Enteties/Enemies/Mine.cs
@@ -5,12 +5,18 @@ public class Mine : Enemy
 {
     [SerializeField] float _timeToBoom = 1f;
     [SerializeField] float _explosionRadius = 2f;
+    [SerializeField] int _maxDamage = 10;
 
+    Animator _animator;
+    int _blinkSpeedParameter;
+    int _blinkTriggerParameter;
     bool _countdownActive = false;
 
     void Start()
     {
-        
+        _animator = GetComponent<Animator>();
+        _blinkSpeedParameter = Animator.StringToHash("Speed");
+        _blinkTriggerParameter = Animator.StringToHash("Blink");
     }
 
     void Update()
@@ -22,19 +28,36 @@ public class Mine : Enemy
     {
         if(_countdownActive == false)
         {
-            StartCoroutine(Kaboom());
+            StartCoroutine(Countdown());
         }    
     }
 
-    IEnumerator Kaboom() {
+    IEnumerator Countdown() {
         _countdownActive = true;
         float timeStarted = Time.time;
-        Debug.Log("Mine started countdown");
+        _animator.SetTrigger(_blinkTriggerParameter);
+        var speed = 1f;
         while(Time.time < timeStarted + _timeToBoom)
         {
             yield return new WaitForEndOfFrame();
+            _animator.SetFloat(_blinkSpeedParameter, speed);
+            speed += Time.deltaTime;
         }
 
-        Debug.Log("Boom! " + _explosionRadius);
+        Kaboom();
+    }
+
+    void Kaboom() {
+        var colliders = Physics2D.OverlapCircleAll(transform.position, _explosionRadius);
+        foreach(Collider2D col in colliders) {
+            var health = col.gameObject.GetComponent<Health>();
+            if(health) 
+            {
+                var distance = transform.position - (Vector3) col.ClosestPoint(transform.position);
+                var damage = distance.magnitude / _explosionRadius * _maxDamage; 
+                health.TakeDamage((int) damage);
+            }
+        }
+        Destroy(gameObject);
     }
 }
